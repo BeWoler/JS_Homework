7cb171dd22274efe51427974b66973a891e393a5
Misha Bireta <46748044+BeWoler@users.noreply.github.com>
2019-10-28 20:10:10 +0300

    Update Homework 8.js

--- Homework 8/Homework 8.js
+++ Homework 8/Homework 8.js
@@ -20,53 +20,30 @@ function Animal(name) {
     this.feed = function() {
         console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
     };
-
-    this.animalFeed = function() {
-        console.log('У каждого животного свой корм');
-    }
 }
 
 function Cat(name) {
     Animal.call(this, name);
 
-    this.paws = 4;
-
     function catHappy() {
         console.log('Кот доволен ^_^');
     };
 
-    var catLike = this.feed;
+   
 
     var self = this;
 
-    self.feed = function() {
-        catLike.call(this);
-        catHappy();
-        return self;
-    };
+    
 
     self.stroke = function() {
         console.log('Гладим кота');
         return self;
     };
 
-    var whiskas = this.animalFeed;
-
-    function catFeed() {
-        console.log('Для котиков это whiskas ');
-    }
     self.animalFeed = function() {
-        whiskas.call(this);
-        catFeed();
+        this.feed.call(this);
         return self;
     }
 }
 
 var barsik = new Cat('Barsik');
-
-console.log(barsik.name);
-console.log(barsik.dailyNorm(49));
-console.log(barsik.feed());
-console.log(barsik.stroke().feed().stroke().stroke().feed().feed());
-console.log(barsik.dailyNorm(250));
-console.log(barsik.feed());
